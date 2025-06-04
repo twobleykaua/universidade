4a4240f1325a888e7639f2ca8e8dc45a7d62c668
twobleykaua <159496665+twobleykaua@users.noreply.github.com>
2025-06-04 23:59:02 +0000

    aula de multi

--- python/gta 6/jogadores/cavaleiro.py
+++ python/gta 6/jogadores/cavaleiro.py
@@ -5,24 +5,24 @@ class Cavaleiro(Jogador):
         super().__init__(nome, dano)
 
 
-        self.__saude = 100 #encapsulamento
+    # self.__saude = 100 #encapsulamento
 
-        @property #decorador retorna apenas como propriedade
-        def saude(self):
-            return self.__saude
+    @property #decorador retorna apenas como propriedade
+    def saude(self):
+         return self.__saude
 
-        @saude.setter #decorador retorna apenas como propriedade
-        def saude(self, valor):
-             self.saude += max(0, valor)
+    @saude.setter #decorador retorna apenas como propriedade
+    def saude(self, valor):
+         self.saude += max(0, valor)
             
-        def atacar(self):
-            print(f"{self.nome} atacou!")
-            print("atacar Polimorfico")
+    def atacar(self):
+         print(f"{self.nome} atacou!")
+         print("atacar Polimorfico")
  
-        def defender(self):
-                print(f"{self.nome} defendeu!")
-                print("defender Polimorfico")
+    def defender(self):
+         print(f"{self.nome} defendeu!")
+         print("defender Polimorfico")
 
 if __name__ == '__main__':
-        cavaleiro = Cavaleiro("rei Artur, 80")
-        print(cavaleiro.atacar())
+     cavaleiro = Cavaleiro("rei Artur", 80)
+     cavaleiro.atacar()
